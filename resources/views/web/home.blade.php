b5cb586a55464a16fb6166b1989c5b9ad0285c1d
MAC <apple@MACs-MacBook-Pro.local>
2018-12-20 08:42:38 -0300

    Cambio color en background de productos de la home

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -17,18 +17,19 @@
                 @forelse($productos as $producto)
 
                     <div>
-                        <a href="{{ route('web.producto.detalle', $producto->id) }}" title="{!! $producto->name !!}">
+                        <a href="{{ route('web.producto.detalle', $producto->id) }}" title="{!! $producto->name !!}" style="background-color:white">
                             <figure>
                                 @if($producto->mainImage())
                                     <img src="{{ route('imagenes.ver', $producto->mainImage()->path) }}" class="img-responsive" alt="{!! $producto->mainImage()->title !!}" style="margin: 0px auto;">
                                 @endif
                             </figure>
-                            <h4>{!! $producto->name !!}</h4>
+                            <h4 style="color:grey">{!! $producto->name !!}</h4>
                         </a>
                     </div>
 
                 @empty
 
+                    {{--Productos de muestra por default--}}
                     <div><figure><img src="{{ asset('template-web/assets/img/case_study_thumb_1.jpg') }}" alt=""> </figure><h4>Producto 1</h4></div>
                     <div><figure><img src="{{ asset('template-web/assets/img/case_study_thumb_2.jpg') }}" alt=""> </figure><h4>Producto 2</h4></div>
                     <div><figure><img src="{{ asset('template-web/assets/img/case_study_thumb_3.jpg') }}" alt=""> </figure><h4>Producto 3</h4></div>
